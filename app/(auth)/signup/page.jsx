fe15ddac3ef56e12448e9f709e2d512c97cd622c
Dzenoo <frontprogramer@gmail.com>
2023-06-11 19:16:45 +0200

    Adding custom hook

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,8 +1,36 @@
+"use client";
+
 import SignupForm from "@/components/Auth/SignupForm";
-// import classes from "@/styles/Auth/Signup.module.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import { useHttpPost } from "@/hooks/Http/useHttpPost";
+import { ClipLoader } from "react-spinners";
 
 const Signup = () => {
-  return <SignupForm />;
+  const { sendPostRequest, isLoading, error } = useHttpPost();
+
+  const submitSignup = async (enteredData) => {
+    await sendPostRequest("", "", enteredData);
+  };
+
+  if (isLoading) {
+    return (
+      <div className="loader_wrapper">
+        <ClipLoader />
+      </div>
+    );
+  }
+
+  if (error) {
+    toast.error(error);
+  }
+
+  return (
+    <>
+      <ToastContainer />
+      <SignupForm submitSignupHandler={submitSignup} />;
+    </>
+  );
 };
 
 export default Signup;
