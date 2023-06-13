5684f8a892c4947aea154a017eab859831d2d672
Dzenoo <frontprogramer@gmail.com>
2023-06-13 20:33:00 +0200

    Lean code

--- components/tasks/ListType.jsx
+++ components/tasks/ListType.jsx
@@ -4,70 +4,35 @@ import { Box, Button, Typography } from "@mui/material";
 import TaskList from "@/components/tasks/TaskList";
 import PropTypes from "prop-types";
 import classes from "@/styles/tasks/list.module.css";
+import { useState } from "react";
 
 const ListType = ({ todoTasks, workTasks, lagTasks, doneTasks }) => {
+  const [listColumns, setListColumns] = useState([
+    { id: "s1", title: "Todo", tasks: todoTasks, color: "#7638dc" },
+    { id: "s2", title: "Work", tasks: workTasks, color: "#daa000" },
+    { id: "s3", title: "Lag", tasks: lagTasks, color: "#ff4229" },
+    { id: "s4", title: "Done", tasks: doneTasks, color: "#1cc800" },
+  ]);
+
   return (
     <Box className={classes.list_tasks_container}>
-      <Box className={classes.list_task_status}>
-        <Box className={classes.list_task_add}>
-          <Typography color="#7638dc" variant="p" fontWeight="bold">
-            Todo
-          </Typography>
-          <Button fontWeight="bold" variant="contained">
-            +
-          </Button>
+      {listColumns.map((li) => (
+        <Box key={li.id} className={classes.list_task_status}>
+          <Box className={classes.list_task_add}>
+            <Typography color={li.color} variant="p" fontWeight="bold">
+              {li.title}
+            </Typography>
+            <Button fontWeight="bold" variant="contained">
+              +
+            </Button>
+          </Box>
+          <Box className={classes.list_task_cards}>
+            {li.tasks.map((todo) => (
+              <TaskList key={todo.id} task={todo} />
+            ))}
+          </Box>
         </Box>
-        <Box className={classes.list_task_cards}>
-          {todoTasks.map((todo) => (
-            <TaskList key={todo.id} task={todo} />
-          ))}
-        </Box>
-      </Box>
-      <Box className={classes.list_task_status}>
-        <Box className={classes.list_task_add}>
-          <Typography color="#7638dc" variant="p" fontWeight="bold">
-            Work
-          </Typography>
-          <Button fontWeight="bold" variant="contained">
-            +
-          </Button>
-        </Box>
-        <Box className={classes.list_task_cards}>
-          {workTasks.map((work) => (
-            <TaskList key={work.id} task={work} />
-          ))}
-        </Box>
-      </Box>
-      <Box className={classes.list_task_status}>
-        <Box className={classes.list_task_add}>
-          <Typography color="#7638dc" variant="p" fontWeight="bold">
-            Lag
-          </Typography>
-          <Button fontWeight="bold" variant="contained">
-            +
-          </Button>
-        </Box>
-        <Box className={classes.list_task_cards}>
-          {lagTasks.map((lag) => (
-            <TaskList key={lag.id} task={lag} />
-          ))}
-        </Box>
-      </Box>
-      <Box className={classes.list_task_status}>
-        <Box className={classes.list_task_add}>
-          <Typography color="#7638dc" variant="p" fontWeight="bold">
-            Done
-          </Typography>
-          <Button fontWeight="bold" variant="contained">
-            +
-          </Button>
-        </Box>
-        <Box className={classes.list_task_cards}>
-          {doneTasks.map((done) => (
-            <TaskList key={done.id} task={done} />
-          ))}
-        </Box>
-      </Box>
+      ))}
     </Box>
   );
 };
