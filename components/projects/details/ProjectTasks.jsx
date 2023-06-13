09866b97b37334b4fc6bca27ca26e1295061134f
Dzenoo <frontprogramer@gmail.com>
2023-06-13 16:24:29 +0200

    Adding project types

---
+++ components/projects/details/ProjectTasks.jsx
@@ -0,0 +1,65 @@
+import KanbanType from "@/components/tasks/KanbanType";
+import ListType from "@/components/tasks/ListType";
+import Image from "next/image";
+import { Box, Button } from "@mui/material";
+
+const ProjectTasks = ({
+  setisTypeTask,
+  isTypeTask,
+  classes,
+  workTasks,
+  doneTasks,
+  todoTasks,
+  lagTasks,
+}) => {
+  return (
+    <Box className={classes.main_tasks_dashboard}>
+      <Box className={classes.show_actions}>
+        <Button
+          onClick={() => setisTypeTask("kanban")}
+          variant={isTypeTask === "kanban" && "contained"}
+        >
+          <Image
+            src="/images/graphic/kanban.png"
+            width={40}
+            height={40}
+            alt="img"
+          />
+          Kanban
+        </Button>
+        <Button
+          size="large"
+          onClick={() => setisTypeTask("list")}
+          variant={isTypeTask === "list" && "contained"}
+        >
+          <Image
+            src="/images/graphic/list.png"
+            width={40}
+            height={40}
+            alt="img"
+          />
+          List View
+        </Button>
+      </Box>
+      <br />
+      <hr />
+      {isTypeTask === "kanban" ? (
+        <KanbanType
+          workTasks={workTasks}
+          doneTasks={doneTasks}
+          todoTasks={todoTasks}
+          lagTasks={lagTasks}
+        />
+      ) : (
+        <ListType
+          workTasks={workTasks}
+          doneTasks={doneTasks}
+          todoTasks={todoTasks}
+          lagTasks={lagTasks}
+        />
+      )}
+    </Box>
+  );
+};
+
+export default ProjectTasks;
