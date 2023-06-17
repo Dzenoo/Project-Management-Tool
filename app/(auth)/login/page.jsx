fa84a97923a311031387e934d4bc85c963fc1a4b
Dzenoo <frontprogramer@gmail.com>
2023-06-17 20:22:55 +0200

    Adding login

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -5,12 +5,29 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 import { useHttpPost } from "@/hooks/Http/useHttpPost";
 import { ClipLoader } from "react-spinners";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/hooks/Auth/useAuth";
+import { useEffect } from "react";
 
 const Login = () => {
+  const { login } = useAuth();
   const { sendPostRequest, isLoading, error } = useHttpPost();
+  const router = useRouter();
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    if (user.token) {
+      router.replace("/");
+    }
+  }, []);
 
   const submitLogin = async (enteredData) => {
-    await sendPostRequest("", "", enteredData);
+    const response = await sendPostRequest(
+      "/api/auth/login",
+      "POST",
+      enteredData
+    );
+    login(response.token);
   };
 
   if (isLoading) {
