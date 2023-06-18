6ae6cddc7e93610111a4e9e92e968f8bb3b5159b
Dzenoo <frontprogramer@gmail.com>
2023-06-18 17:35:24 +0200

    Adding create team form

--- app/(team)/team/page.jsx
+++ app/(team)/team/page.jsx
@@ -3,16 +3,42 @@
 import classes from "@/styles/team/team.module.css";
 import { teams } from "@/data/tasks.jsonData.config.json";
 import TeamCard from "@/components/team/TeamCard";
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, TextField, Typography, Button } from "@mui/material";
+import MainModal from "@/components/shared/MainModal";
+import { useState } from "react";
 
 const TeamPage = () => {
+  const [isOpenModal, setisOpenModal] = useState(false);
+  const closeModal = () => setisOpenModal(false);
+  const openModal = () => setisOpenModal(true);
+
+  const create = (
+    <form className={classes.create_team_form}>
+      <TextField fullWidth label="Name" />
+      <TextField fullWidth label="Description" multiline rows={3} />
+      <Button variant="contained">Create</Button>
+    </form>
+  );
+
   return (
     <Box className={classes.team_wrapper}>
+      <MainModal
+        isOpen={isOpenModal}
+        close={closeModal}
+        title="Create Team"
+        text="Create team, start collaborating"
+        content={create}
+      />
       <Box className={classes.team_actions}>
         <Typography variant="h4" fontWeight="bold">
           Teams
         </Typography>
-        <TextField placeholder="Search Teams..." label="Search" />
+        <div className={classes.team_actions_btns}>
+          <Button onClick={openModal} variant="contained">
+            Create Team
+          </Button>
+          <TextField placeholder="Search Teams..." label="Search" />
+        </div>
       </Box>
       <Box className={classes.team_container}>
         {teams.map((team) => (
