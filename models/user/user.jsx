158e5e9ab0670aca50b3b7ba5f9ddac52dd7acd4
Dzenoo <frontprogramer@gmail.com>
2023-06-18 17:59:23 +0200

    Adding user schema

--- models/user/user.jsx
+++ models/user/user.jsx
@@ -1,12 +1,18 @@
-const { default: mongoose, models, model } = require("mongoose");
+import { Schema, model, models } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   first_name: { type: String, required: [true, "First Name is required"] },
   last_name: { type: String, required: [true, "Last Name is required"] },
   username: { type: String, required: [true, "Username is required"] },
   email: { type: String, required: [true, "Email is required"] },
   password: { type: String, required: [true, "Password is required"] },
   image: { type: String, required: [true, "Image is required"] },
+  github: { type: String },
+  linkedin: { type: String },
+  website: { type: String },
+  biography: { type: String },
+  address: { type: String },
+  team: [{ type: Schema.Types.ObjectId, ref: "Team" }],
 });
 
 const User = models.User || model("User", UserSchema);
