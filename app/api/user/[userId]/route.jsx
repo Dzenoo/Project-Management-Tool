32e8f14bb88a6b7288fa43ab2528a0641c2d341b
Dzenoo <frontprogramer@gmail.com>
2023-06-19 15:12:18 +0200

    Fetching user

--- app/api/user/[userId]/route.jsx
+++ app/api/user/[userId]/route.jsx
@@ -7,7 +7,16 @@ export const GET = async (request, { params }) => {
   try {
     await connectToDB();
 
-    const user = await User.findById(params.userId).populate("teams.team");
+    const user = await User.findById(params.userId)
+      .select("-password")
+      .populate({
+        path: "teams.team",
+        populate: {
+          path: "teamMembers.user",
+          model: "User",
+          select: "first_name last_name email image github linkedin",
+        },
+      });
 
     return new Response(JSON.stringify(user), { status: 200 });
   } catch (error) {
