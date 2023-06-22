e1da3a449b0ef264ee317ab62b19465b4c132bb1
Dzenoo <frontprogramer@gmail.com>
2023-06-22 13:56:15 +0200

    fixing models

--- models/shared/Team.jsx
+++ models/shared/Team.jsx
@@ -1,12 +1,15 @@
 import { Schema, model, models } from "mongoose";
 
+import User from "../user/user";
+import Project from "../projects/project";
+
 const TeamSchema = new Schema(
   {
     name: { type: String, required: [true, "Name is required"] },
     description: { type: String, required: [true, "Description is required"] },
     image: { type: String },
-    teamMembers: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    projects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+    teamMembers: [{ type: Schema.Types.ObjectId, ref: User }],
+    projects: [{ type: Schema.Types.ObjectId, ref: Project }],
   },
   { timestamps: true }
 );
