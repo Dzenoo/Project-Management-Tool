d3b0a3d010ddc650d979cd1758c1e32d93a5e352
Dzenoo <frontprogramer@gmail.com>
2023-06-23 22:57:59 +0200

    useFetch

--- hooks/Http/useFetch.jsx
+++ hooks/Http/useFetch.jsx
@@ -3,10 +3,7 @@ import useSwr from "swr";
 export const useFetch = (path) => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, error } = useSwr(path, fetcher, {
-    revalidateOnMount: true,
-    refreshInterval: false,
-  });
+  const { data, error } = useSwr(path, fetcher);
 
   return { data, error };
 };
