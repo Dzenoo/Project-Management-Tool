9441dff354abb69f98e9370b31b4e5bb8818322a
Dzenoo <frontprogramer@gmail.com>
2023-06-24 14:48:48 +0200

    Fixing fetch

--- hooks/Http/useFetch.jsx
+++ hooks/Http/useFetch.jsx
@@ -3,7 +3,10 @@ import useSwr from "swr";
 export const useFetch = (path) => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, error } = useSwr(path, fetcher);
+  const { data, error } = useSwr(path, fetcher, {
+    refreshInterval: true,
+    revalidateOnMount: true,
+  });
 
   return { data, error };
 };
