056d58598c601a586c4b282b0ee75b7bb8b4ec75
Dzenoo <frontprogramer@gmail.com>
2023-06-24 13:23:12 +0200

    Removing layout

--- app/(team)/team/layout.jsx
+++ app/(team)/team/layout.jsx
@@ -1,19 +1,6 @@
 "use client";
-import { AppContext } from "@/context/AppContext";
-import { Typography } from "@mui/material";
-import { useContext } from "react";
 
 const layout = ({ children }) => {
-  const { userInfo } = useContext(AppContext);
-
-  if (!userInfo || userInfo === undefined) {
-    return (
-      <Typography textAlign="center" mt={2} fontWeight="bold">
-        Please log in or sign up
-      </Typography>
-    );
-  }
-
   return (
     <div className="main_layout">
       <div className="content">{children}</div>
